744f55654cb8bfcdcb93ab0f6811d8556b4dc0b8
card12-chat <paulocardoso.vitor78@gmail.com>
2026-04-14 14:58:44 -0300

    fix(tracking): refactor tracking script to GA4 ecommerce datalayer push

--- parse_serve_vars.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let srvData = JSON.parse(fs.readFileSync('TRACKEAMENTO/Server-Yampi.json', 'utf8'));
-
-let tag = srvData.containerVersion.tag.find(t => t.name === "02 | FB | Purchase");
-
-console.log(JSON.stringify(tag.parameter.filter(p=>p.key==="userDataList" || p.key==="customDataList" || p.key==="serverEventDataList"), null, 2));
-
